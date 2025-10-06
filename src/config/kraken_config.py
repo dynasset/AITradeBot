94d93b9f829fa16cfaf6cae38776090e30f9bf88
S. Koopman <116167079+dynasset@users.noreply.github.com>
2025-10-06 11:15:55 +0000

    Opschonen dubbele mappen: alleen src/ai_tradebot structuur behouden

--- src/config/kraken_config.py
+++
@@ -1,8 +0,0 @@
-# Configuratie voor het laden van environment variables
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-KRAKEN_API_KEY = os.getenv("KRAKEN_API_KEY")
-KRAKEN_API_SECRET = os.getenv("KRAKEN_API_SECRET")
